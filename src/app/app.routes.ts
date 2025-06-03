359290fc7f5d5a52bec3733fa23d6201edf8145c
Casey Hald <hald.casey@gmail.com>
2025-06-03 09:14:06 -0700

    updated theme vars

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { RangeSelectorComponent } from './components/range-selector/range-selector.component';
 import { PersonaGridComponent } from './components/persona-grid/persona-grid.component';
+import { SelectInputComponent } from './components/select-input/select-input.component';
 
 export const routes: Routes = [
   { path: 'range-selector', component: RangeSelectorComponent },
   { path: 'persona-grid', component: PersonaGridComponent },
+  { path: 'select-input', component: SelectInputComponent },
   { path: '', redirectTo: '/range-selector', pathMatch: 'full' },
 ];
---
+++ src/app/components/select-input/select-input.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { SelectModule } from 'primeng/select';
+import { cascadeFigmaVariables } from '../../cascade';
+
+interface Conditional {
+  name: string;
+  code: string;
+}
+
+@Component({
+  selector: 'app-select-input',
+  imports: [SelectModule],
+  templateUrl: './select-input.component.html',
+  styleUrl: './select-input.component.css'
+})
+
+export class SelectInputComponent {
+  colors = cascadeFigmaVariables;
+  conditionals: Conditional[] | undefined;
+
+  selectedConditional: Conditional | undefined;
+  ngOnInit() {
+    this.conditionals = [
+      { name: 'Between', code: 'Between' },
+      { name: 'Equals', code: 'Equals' },
+      { name: 'More Than', code: 'More Than' },
+      { name: 'Less Than', code: 'Less Than' },
+      { name: 'Does Not Equal', code: 'Does Not Equal' },
+    ];
+  }
+}
